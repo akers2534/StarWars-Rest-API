9d98041787654da3d931cd3f4f9d27c872e41907
akers2534 <akers2534@gmail.com>
2023-02-25 21:47:53 +0000

    API 1/2

--- src/models.py
+++ src/models.py
@@ -8,6 +8,8 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+    
+
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -15,5 +17,61 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    height = db.Column(db.String(25), unique=False, nullable=False)
+    weight = db.Column(db.String(20), unique=False, nullable=False)
+    gender = db.Column(db.String(20), unique=False, nullable=False)
+    planets_id = db.Column(db.Integer, db.ForeignKey("planets.id"), nullable=False)
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "weight": self.weight,
+            "gender": self.gender,
+            # do not serialize the password, its a security breach
+        }
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    terrain = db.Column(db.String(150), unique=False, nullable=False)
+    gravity = db.Column(db.String(150),unique=False, nullable=False)
+    name = db.Column(db.String(150), unique=False, nullable=False)
+    population = db.Column(db.Integer, unique=False, nullable=False)
+    people = db.relationship("People", backref="Planets", lazy=True)
+    
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "terrain": self.terrain,
+            "population": self.population,
+            "gravity": self.gravity,
+            # do not serialize the password, its a security breach
+        }
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planets = db.relationship("Planets", backref="favorites", lazy=True)     
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "terrain": self.terrain,
+            "population": self.population,
+            "gravity": self.gravity,
             # do not serialize the password, its a security breach
         }
